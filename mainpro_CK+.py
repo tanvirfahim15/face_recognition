cd51c5f01b40f242d57ed07db0996f86015071c0
tfahim15 <66311241+tfahim15@users.noreply.github.com>
2022-08-26 13:03:09 +0600

    first commit

--- mainpro_CK+.py
+++ mainpro_CK+.py
@@ -114,7 +114,7 @@ def train(epoch):
         utils.clip_gradient(optimizer, 0.1)
         optimizer.step()
 
-        train_loss += loss.data[0]
+        train_loss += loss.data
         _, predicted = torch.max(outputs.data, 1)
         total += targets.size(0)
         correct += predicted.eq(targets.data).cpu().sum()
@@ -143,7 +143,7 @@ def test(epoch):
         outputs_avg = outputs.view(bs, ncrops, -1).mean(1)  # avg over crops
 
         loss = criterion(outputs_avg, targets)
-        PrivateTest_loss += loss.data[0]
+        PrivateTest_loss += loss.data
         _, predicted = torch.max(outputs_avg.data, 1)
         total += targets.size(0)
         correct += predicted.eq(targets.data).cpu().sum()
